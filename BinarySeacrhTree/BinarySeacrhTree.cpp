104b5191006561293ad31adc8d554b7bbd81ed8e
Sulthonhanan <hananakhnaf31@gmail.com>
2024-06-13 15:45:03 +0700

    Commit 9

--- BinarySeacrhTree/BinarySeacrhTree.cpp
+++ BinarySeacrhTree/BinarySeacrhTree.cpp
@@ -106,4 +106,11 @@ public:
             cout << "Tree is empty" << endl;
             return;
         }
-        
+        if (ptr != nullptr)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
