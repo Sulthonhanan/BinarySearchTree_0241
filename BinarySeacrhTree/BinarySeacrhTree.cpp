f192391536eb40e057e242fa175c27df874f2215
Sulthonhanan <hananakhnaf31@gmail.com>
2024-06-13 15:19:06 +0700

    Commit 3

--- BinarySeacrhTree/BinarySeacrhTree.cpp
+++ BinarySeacrhTree/BinarySeacrhTree.cpp
@@ -38,4 +38,13 @@ public:
         newNode->leftchild = nullptr; // make the left child of the new node point to NULL
         newNode->rightchild = nullptr; // make the right child of the new node point to NULL
 
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // Locate the node which will be the parent of the node to be insert
+
+        if (parent == nullptr) // if the parent is NULL (tree is empty)
+        {
+            ROOT = newNode; // mark the new node as root
+            return; // exit
+        }
        
